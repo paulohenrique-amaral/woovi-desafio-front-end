0822b30fbdcd97e1b6c0c576162d082388a5b809
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-10 19:02:50 -0300

    feat: instalation lib material UI, and created theme and implementation theme

---
+++ src/themes/default.ts
@@ -0,0 +1,29 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#03D69D ',
+      contrastText: '#4D4D4D',
+    },
+    secondary: {
+      main: '#133A6F',
+      light: '#B2B2B2',
+    },
+    success: {
+      main: '#03D69D',
+    },
+    background: {
+      default: '#FFFFFF',
+      paper: '#E5E5E5',
+    },
+    text: {
+      primary: '#4D4D4D',
+      secondary: '#AFAFAF',
+    },
+  },
+});
+
+const theme = responsiveFontSizes(defaultTheme);
+
+export default theme;
