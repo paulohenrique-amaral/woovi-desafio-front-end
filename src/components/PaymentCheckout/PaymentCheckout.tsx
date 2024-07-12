977e5c80cf88071c3a87ef71f88057e1e646da9c
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-12 10:27:24 -0300

    feat: created PaymentCheckout

---
+++ src/components/PaymentCheckout/PaymentCheckout.tsx
@@ -0,0 +1,12 @@
+import { Container, Grid, Box, Typography, styled, keyframes } from '@mui/material';
+import PaymentCheckoutMsg from '../PaymentCheckoutMsg/PaymentCheckoutMsg';
+
+function PaymentCheckout() {
+  return (
+    <Grid container>
+      <PaymentCheckoutMsg />
+    </Grid>
+  );
+}
+
+export default PaymentCheckout;
