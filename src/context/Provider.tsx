a3409b4e5830fd6b3e3bcc14c2b9545d6aba8194
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-12 10:25:00 -0300

    feat: update provider, adding checkout state and created type

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,16 +1,36 @@
 import { useState } from 'react';
-import { ProviderProps, ProviderValues } from '../types/types';
+import { ProviderProps, ProviderValues, typeCheckout } from '../types/types';
 import Context from './Context';
+
+const initialState: typeCheckout = {
+  client: '',
+  installmentNumber: 0,
+  installmentPrice: 0,
+  creditData: {
+    name: '',
+    cpf: '',
+    creditCard: '',
+    cvv: '',
+    expirationDate: '',
+  },
+};
 
 function Provider({ children }: ProviderProps) {
   const [total, setTotal] = useState(0);
   const [paymentStage, setPaymentStage] = useState<number | null>(null);
+  const [checkout, setCheckout] = useState<typeCheckout>(initialState);
+
+  function updateCheckout(key: keyof typeCheckout, value: any) {
+    setCheckout(prev => ({ ...prev, [key]: value }));
+  }
 
   const value: ProviderValues = {
     total,
     setTotal,
     paymentStage,
     setPaymentStage,
+    checkout,
+    updateCheckout,
   };
 
   return (
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,23 @@
+export type typeCheckout = {
+  client: string;
+  installmentNumber?: number;
+  installmentPrice?: number;
+  creditData: {
+    name: string;
+    cpf: string;
+    creditCard: string;
+    cvv: string;
+    expirationDate: string;
+  };
+};
+
 export type ProviderValues = {
   total: number;
   setTotal: (value: number) => void;
   paymentStage: number | null;
   setPaymentStage: (value: number | null) => void;
+  checkout: typeCheckout | undefined;
+  updateCheckout: (key: keyof typeCheckout, value: any) => void;
 };
 
 export type ProviderProps = {
