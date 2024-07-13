64b814488cd41f2963e6c15845f6d3f75efcb7f6
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-13 19:56:15 -0300

    refactor: refactor PaymentMethod and created style file

--- src/pages/PaymentMethod/PaymentMethod.tsx
+++ src/pages/PaymentMethod/PaymentMethod.tsx
@@ -1,44 +1,15 @@
 import { useContext, useState, useEffect } from 'react';
-import { Container, Grid, Box, Typography, styled, keyframes } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import Context from '../../context/Context';
-import PaymentCard from '../../components/PaymentCard/PaymentCard';
 import PaymentOptions from '../../components/PaymentOptions/PaymentOptions';
 import PaymentCheckout from '../../components/PaymentCheckout/PaymentCheckout';
-import PaymentFormCreditCard from '../../components/PaymentFormCreditCard/PaymentFormCreditCard';
-
-const slideInRight = keyframes`
-  0% {
-    -webkit-transform: translateX(1000px);
-            transform: translateX(1000px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateX(0);
-            transform: translateX(0);
-    opacity: 1;
-  }
-`;
-
-const slideOutRight = keyframes`
-  0% {
-    -webkit-transform: translateX(0);
-            transform: translateX(0);
-    opacity: 1;
-  }
-  100% {
-    -webkit-transform: translateX(-1000px);
-            transform: translateX(-1000px);
-    opacity: 0;
-  }
-`;
-
-const BoxStyled = styled(Box)<{ animation: string }>(({ theme, animation }) => ({
-  animation: `${animation} 0.9s`,
-}));
+import PaymentFormCreditCard
+  from '../../components/PaymentFormCreditCard/PaymentFormCreditCard';
+import { slideInRight, slideOutRight, BoxStyled } from './PaymentMethodStyled';
 
 function PaymentMethod() {
   const [animation, setAnimation] = useState(slideInRight);
-  const {paymentStage, setPaymentStage, updateCheckout, setTotal} = useContext(Context);
+  const { paymentStage, updateCheckout, setTotal } = useContext(Context);
   const [showPaymentOptions, setShowPaymentOptions] = useState(true);
   const [showPaymentCheckout, setShowPaymentCheckout] = useState(false);
   const [showPaymentFormCreditCard, setShowPaymentFormCreditCard] = useState(false);
@@ -46,8 +17,7 @@ function PaymentMethod() {
   useEffect(() => {
     updateCheckout('client', 'João');
     setTotal(30500);
-    setPaymentStage(1);
-  }, [setPaymentStage]);
+  }, [setTotal]);
 
   useEffect(() => {
     if (paymentStage === 1) {
@@ -55,61 +25,57 @@ function PaymentMethod() {
       setShowPaymentCheckout(false);
       setAnimation(slideInRight);
     } else if (paymentStage === 2) {
-      setAnimation(slideOutRight);  
+      setAnimation(slideOutRight);
       setTimeout(() => {
         setShowPaymentOptions(false);
         setShowPaymentCheckout(true);
         setAnimation(slideInRight);
       }, 900);
     } else if (paymentStage === 3) {
-      setAnimation(slideOutRight);  
+      setAnimation(slideOutRight);
       setTimeout(() => {
         setShowPaymentCheckout(false);
         setShowPaymentFormCreditCard(true);
         setAnimation(slideInRight);
       }, 900);
     }
-
   }, [paymentStage]);
-  
 
   return (
     <Container maxWidth="lg">
       <Grid container>
         <Grid item>
-        {showPaymentOptions && (
-          <Grid
-            item
-            xs={12}
-            md={12}
-            // sx={{ overflow: 'auto' }}
-          >
-            <BoxStyled animation={animation}>
-              <PaymentOptions />
-            </BoxStyled>
-          </Grid>            
-        )}
-        {showPaymentCheckout && (
-          <Grid
-          item
-          xs={12}
-          // md={12}sx={{ maxHeight: '100%' }}
-        >
-          <BoxStyled animation={animation}>
-            <PaymentCheckout />
-          </BoxStyled>
-        </Grid>            
-        )}
-        {showPaymentFormCreditCard && (
-          <Grid
-          item
-          xs={12}
-        >
-          <BoxStyled animation={animation}>
-            <PaymentFormCreditCard />
-          </BoxStyled>
-        </Grid>            
-        )}
+          {showPaymentOptions && (
+            <Grid
+              item
+              xs={ 12 }
+              md={ 12 }
+            >
+              <BoxStyled animation={ animation }>
+                <PaymentOptions />
+              </BoxStyled>
+            </Grid>
+          )}
+          {showPaymentCheckout && (
+            <Grid
+              item
+              xs={ 12 }
+            >
+              <BoxStyled animation={ animation }>
+                <PaymentCheckout />
+              </BoxStyled>
+            </Grid>
+          )}
+          {showPaymentFormCreditCard && (
+            <Grid
+              item
+              xs={ 12 }
+            >
+              <BoxStyled animation={ animation }>
+                <PaymentFormCreditCard />
+              </BoxStyled>
+            </Grid>
+          )}
         </Grid>
       </Grid>
     </Container>
---
+++ src/pages/PaymentMethod/PaymentMethodStyled.ts
@@ -0,0 +1,31 @@
+import { Box, styled, keyframes } from '@mui/material';
+
+export const slideInRight = keyframes`
+  0% {
+    -webkit-transform: translateX(1000px);
+            transform: translateX(1000px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateX(0);
+            transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+export const slideOutRight = keyframes`
+  0% {
+    -webkit-transform: translateX(0);
+            transform: translateX(0);
+    opacity: 1;
+  }
+  100% {
+    -webkit-transform: translateX(-1000px);
+            transform: translateX(-1000px);
+    opacity: 0;
+  }
+`;
+
+export const BoxStyled = styled(Box)<{ animation: string }>(({ theme, animation }) => ({
+  animation: `${animation} 0.9s`,
+}));
